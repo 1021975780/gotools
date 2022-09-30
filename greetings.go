b19699eb9cce29dc097a24af6254b41d8d24edc8
王群 <1021975780@qq.com>
2022-09-30 16:47:50 +0800

    Return and handle an error-v0.0.5

--- greetings.go
+++ greetings.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
@@ -15,11 +16,23 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	var resultMap map[string]string = make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		resultMap[name] = message
+	}
+	return resultMap, nil
+}
 func randomFormat() string {
 	var formats []string = []string{
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
 		"Hail, %v! Well met!",
 	}
+	rand.Seed(time.Now().Unix())
 	return formats[rand.Intn(len(formats))]
 }
