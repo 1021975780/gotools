194379d55adf96e99f8c651c6765a860f4fa9870
王群 <1021975780@qq.com>
2022-09-30 15:20:20 +0800

    Return and handle an error-v0.0.3

--- greetings.go
+++ greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("名字不能为空")
+	}
 	message := fmt.Sprintf("Hello world, my name is %v !", name)
 	fmt.Println("v0.0.2")
-	return message
+	return message, nil
 }
