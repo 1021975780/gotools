7d0d0cfc7903d6eeb0c01665b5f7aaabe7208f46
王群 <1021975780@qq.com>
2022-09-30 15:47:42 +0800

    Return and handle an error-v0.0.4

--- greetings.go
+++ greetings.go
@@ -3,13 +3,23 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("名字不能为空")
 	}
-	message := fmt.Sprintf("Hello world, my name is %v !", name)
-	fmt.Println("v0.0.2")
+	//message := fmt.Sprintf("Hello world, my name is %v !", name)
+	var message string = fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	var formats []string = []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
